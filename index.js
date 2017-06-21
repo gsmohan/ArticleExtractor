3b6b11b96b2b36c433f93ebefb4a3131bb1f5828
gsmohan <mohangs89@gmail.com>
2017-06-21 13:30:10 +0530

    Refactoring and adding directories

--- index.js
+++ index.js
@@ -1,96 +1,118 @@
-let querystring = require('querystring');
-let http = require('http');
-let jsdom = require("node-jsdom");
-let pdf = require('html-pdf');
-let fs = require('fs');
+let querystring = require('querystring'),
+    http = require('http'),
+    jsdom = require("node-jsdom"),
+    pdf = require('html-pdf'),
+    fs = require('fs');
+
+let apiConfig = {
+  host: 'www.prajavani.net',
+  archivePath: '/mobile/archive',
+  articlePath: '/news',
+};
+
+
+let dirName = './articles';
+
 
 //TODO:
 //1 - passing date as a params
-//2 - removing the dupicate places
+// 1.1 - pass the year and month in YYYY-MM format
+// 1.2 - create a dir with arg name only if article is present
+//3 - checking null while DOM searching
+
 
 class PlaceExtractor {
-  constructor(props){
-    //Will get the array of places
-    for(let i=0; i<props.length; i++){
-      this.fetchPlace(props[i])
+
+  constructor(places){
+    for(let i=0; i<places.length; i++){
+      this.fetchPlaceData(places[i])
     }
   }
 
-  fetchPlace(place){
+  createPdf(title, data){
+    if (!fs.existsSync(dirName)){
+        fs.mkdirSync(dirName);
+    }
+    pdf.create(data).toStream((err, stream) => {
+      stream.pipe(fs.createWriteStream(`${dirName}/${title}.pdf`));
+      console.log(`Pdf file saved`);
+    });
+  }
+
+  extractContentFromHtml(title, html){
+    jsdom.env(html, (err, window) => {
+      let documentEl = window.document,
+          holderEl = documentEl.querySelector('body .container #main_container #article_section .article_body'),
+          author = holderEl.querySelector('.article_author'),
+          date = holderEl.querySelector('.article_date'),
+          summary = holderEl.querySelector('.article_summary'),
+          thumbnail = holderEl.querySelector('.article_image'),
+          content = holderEl.querySelector('.body'),
+          gallery = holderEl.querySelector('.my-gallery');
+
+      let data = `<h1>${(title === null)? '' : title}</h1>
+                  ${(author === null)? '' : author.outerHTML}
+                  ${(date === null)? '' : date.outerHTML}
+                  ${(summary === null)? '' : summary.outerHTML}
+                  ${(thumbnail === null)? '' : thumbnail.outerHTML}
+                  ${(content === null)? '' : content.outerHTML}
+                  ${(gallery === null)? '' : gallery.outerHTML}`;
+
+      this.createPdf(title, data);
+
+    });
+  }
+
+  fetchPlaceData(place){
     let { title, url } = place;
     let options = {
-      host: 'www.prajavani.net',
-      path: `/news${url}`
+      host: apiConfig.host,
+      path: `${apiConfig.articlePath}${url}`
     }
 
     http.request(options, (response)=>{
       let placeData = [];
-      response.on('data', function (chunk) {
+      response.on('data', (chunk) => {
         placeData.push(chunk);
       });
 
-      response.on('end', function () {
+      response.on('end', () => {
         placeData = Buffer.concat(placeData).toString();
-        jsdom.env(placeData, (err, window) => {
-          let documentEl = window.document,
-              holderEl = documentEl.querySelector('body .container #main_container #article_section .article_body'),
-              author = holderEl.querySelector('.article_author').outerHTML,
-              date = holderEl.querySelector('.article_date').outerHTML,
-              summary = holderEl.querySelector('.article_summary').outerHTML,
-              thumbnail = holderEl.querySelector('.article_image').outerHTML,
-              content = holderEl.querySelector('.body').outerHTML,
-              gallery = holderEl.querySelector('.my-gallery').outerHTML;
-
-              let doc = `<h1>${title}</h1>
-              <div>${author} @ ${date}</div>
-              ${summary}
-              ${thumbnail}
-              ${content}
-              ${gallery}`;
-
-              pdf.create(doc).toStream((err, stream) => {
-                stream.pipe(fs.createWriteStream(`./articles/${title}.pdf`));
-                console.log(`${title}.pdf file saved`);
-              });
-
-        });
-       // End of jsdom
+        this.extractContentFromHtml(title, placeData);
       });
-      // End of response End
     }).end();
   }
 }
 
 class CategoryExtractor {
+
   constructor(){
-    this.params = querystring.stringify({arcDate: "2017-02-19"});
-    this.archiveOptions = {
-      host: 'www.prajavani.net',
-      path: '/mobile/archive',
+    this.params = querystring.stringify({arcDate: "2016-12-11"});
+    this.options = {
+      host: apiConfig.host,
+      path: apiConfig.archivePath,
       method: 'POST',
       headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           'Content-Length': Buffer.byteLength(this.params)
       }
     }
-
+    this.fetchArchiveData();
   }
 
   parseArchiveData(data){
     let jsonData = JSON.parse(data, null, 2);
-    let prop = 'ಪ್ರವಾಸ';
-    if(jsonData.hasOwnProperty(prop)){
-      //console.log('Found Artical or Place', jsonData[prop]);
-      let placesInfo = jsonData[prop];
-      //TODO: again fetch and save page
+    let category = 'ಪ್ರವಾಸ';
+    if(jsonData.hasOwnProperty(category)){
+      let placesInfo = jsonData[category];
       new PlaceExtractor(placesInfo);
     }else{
-      console.log(`Sorry !! No ${prop} Artical on this Day`);
+      console.log(`Sorry !! No ${category} Article on this Day`);
     }
   }
 
-  fetchArchive(){
-    let request = http.request(this.archiveOptions, (response) => {
+  fetchArchiveData(){
+    let request = http.request(this.options, (response) => {
         let data = '';
         response.on('data', (chunk) => {
             data += chunk.toString();
@@ -105,5 +127,5 @@ class CategoryExtractor {
   }
 }
 
-let App = new CategoryExtractor();
-    App.fetchArchive();
+// Start the App.
+new CategoryExtractor();
